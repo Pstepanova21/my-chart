bf7c02dc0ec8af70948da2a8afd2ec1a76d85267
Polina <polina_stepanova7741@mail.ru>
2024-05-18 00:12:19 +0300

    chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts";
+
+const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+const options = {
+  title: "My Daily Activities",
+  pieHole: 0.4,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "50%", margin: "0 auto", paddingTop: "50px" }}>
+      <h2>Pie Chart Example</h2>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width="100%"
+        height="400px"
+      />
     </div>
   );
 }
